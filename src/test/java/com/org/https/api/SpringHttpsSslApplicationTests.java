69ac76313b7bf7485fd8bb48393798ad9ed315d6
gdmedage <gdmedage@gmail.com>
2020-05-02 15:40:52 +0530

    Renamed the test class

--- src/test/java/com/org/https/api/SpringHttpsSslApplicationTests.java
+++ src/test/java/com/org/https/api/SpringHttpsSslApplicationTests.java
@@ -1,4 +1,4 @@
-package com.javatechie.spring.cache.api;
+package com.org.https.api;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SpringHazlecastCacheApplicationTests {
+public class SpringHttpsSslApplicationTests {
 
 	@Test
 	public void contextLoads() {
